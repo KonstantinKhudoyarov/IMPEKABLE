c8884e68a78c897697373119559e3b4c6ad0c7db
KonstantinKhudoyarov <kostya040390@gmail.com>
2019-08-20 20:12:12 +0300

    begin work with Calendar component

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './MainContent.scss';
-import { Header } from '../Header/Header'
+import { Header } from '../Header/Header';
+import { BigCalendar } from '../Calendar/Calendar';
 
 export class MainContent extends React.Component {
     render() {
         return (
             <main className="main">
                 <Header />
+                <BigCalendar />
             </main>
         );
     }
