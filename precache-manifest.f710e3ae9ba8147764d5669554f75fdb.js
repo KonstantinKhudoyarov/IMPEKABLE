fb92b5fc740c861aefb3d237d8666b51429e6037
KonstantinKhudoyarov <kostya040390@gmail.com>
2019-08-27 12:14:29 +0300

    Updates

---
+++ precache-manifest.f710e3ae9ba8147764d5669554f75fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8d2d83cfd41a6e75551c28a136de63",
+    "url": "/IMPEKABLE/index.html"
+  },
+  {
+    "revision": "aa9f82362a54bbd8bd71",
+    "url": "/IMPEKABLE/static/css/2.900c6d0a.chunk.css"
+  },
+  {
+    "revision": "4a7afc8cab4a45f31ab9",
+    "url": "/IMPEKABLE/static/css/main.efca4dfe.chunk.css"
+  },
+  {
+    "revision": "aa9f82362a54bbd8bd71",
+    "url": "/IMPEKABLE/static/js/2.a01e1b8a.chunk.js"
+  },
+  {
+    "revision": "4a7afc8cab4a45f31ab9",
+    "url": "/IMPEKABLE/static/js/main.a1e038c7.chunk.js"
+  },
+  {
+    "revision": "37077106cc2e9338c4d3",
+    "url": "/IMPEKABLE/static/js/runtime~main.8d36b89f.js"
+  }
+]);
